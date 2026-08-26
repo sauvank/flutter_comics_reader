313a7d465c2666a5aef6cf32fcf7509be5617dd3
Kevin <3721632-sauvanaud.kevin@users.noreply.gitlab.com>
2026-08-26 11:03:32 +0200

    fix: resolve Flutter 3.29 theme type and matrix scale deprecations

--- lib/screens/cbz_reader_screen.dart
+++ lib/screens/cbz_reader_screen.dart
@@ -297,7 +297,7 @@ class _CbzReaderScreenState extends State<CbzReaderScreen> {
       controller.value = Matrix4.identity();
     } else {
       // Zoom in 2x centered
-      final zoomed = Matrix4.identity()..scale(2.0, 2.0);
+      final zoomed = Matrix4.diagonal3Values(2.0, 2.0, 1.0);
       controller.value = zoomed;
     }
   }
