e7fff096edbc2c7afc39a08f59c9b875a9c4b78b
Kevin <3721632-sauvanaud.kevin@users.noreply.gitlab.com>
2026-09-19 23:30:23 +0200

    chore(version): bump version to 2.0.31+86 (v2.0.31)

--- lib/constants/app_version.dart
+++ lib/constants/app_version.dart
@@ -1,6 +1,6 @@
 class AppVersion {
-  static const String version = '2.0.30';
-  static const int buildNumber = 85;
+  static const String version = '2.0.31';
+  static const int buildNumber = 86;
   static const String fullVersion = 'v$version';
   static const String githubRepo = 'sauvank/flutter_comics_reader';
   static const String githubReleasesUrl = 'https://github.com/$githubRepo/releases';
